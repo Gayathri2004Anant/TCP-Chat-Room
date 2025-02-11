6a9fb18fe4ba0d9119119aac88a38d9bce4939ea
Gayathri Anant <134033920+Gayathri2004Anant@users.noreply.github.com>
2025-02-11 22:53:59 +0530

    Update chatclient.c

--- chatclient.c
+++ chatclient.c
@@ -20,7 +20,7 @@ int main(){
     int sockfd;
     struct sockaddr_in serv_addr;
 
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
